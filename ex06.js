5bba62fe7c3012e186d98aabfdfe590d3d20405e
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-03-11 17:52:39 +0100

    added the 4th condition to pass the 4th test

--- ex06.js
+++ ex06.js
@@ -59,8 +59,11 @@ function getHumanTypeByAge(age){
         return "enfant";
     else if (age >= 12 && age < 18)
         return "ado";
+    else if (age >= 18 && age < 124)
+        return "adulte";
 };
 
 console.log(getHumanTypeByAge(-1));
 console.log(getHumanTypeByAge(6));
 console.log(getHumanTypeByAge(17));
+console.log(getHumanTypeByAge(33));
